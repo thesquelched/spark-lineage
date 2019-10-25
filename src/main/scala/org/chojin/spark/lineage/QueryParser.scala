901689a1052df989a95fd176bcf779ad4afbe4c2
Scott Kruger <scott.kruger@getbraintree.com>
2019-10-24 20:50:44 -0500

    Move query parsing logic into QueryParser

---
+++ src/main/scala/org/chojin/spark/lineage/QueryParser.scala
@@ -0,0 +1,105 @@
+package org.chojin.spark.lineage
+
+import grizzled.slf4j.Logger
+import org.apache.spark.sql.catalyst.catalog.HiveTableRelation
+import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeReference}
+import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Filter, Join, LogicalPlan, Project}
+import org.apache.spark.sql.execution.QueryExecution
+import org.apache.spark.sql.execution.datasources.{InsertIntoHadoopFsRelationCommand, LogicalRelation}
+import org.chojin.spark.lineage.inputs.How.How
+import org.chojin.spark.lineage.inputs.{Field, HiveInput, How, Input}
+import org.chojin.spark.lineage.outputs.FsOutput
+import org.chojin.spark.lineage.report.{Metadata, Report}
+
+object QueryParser {
+  private lazy val LOGGER = Logger[this.type]
+
+  def findSource(attr: AttributeReference, plan: LogicalPlan, how: How): Seq[Input] = findSource(attr.toAttribute, plan, how)
+
+  def findSource(attr: Attribute, plan: LogicalPlan, how: How): Seq[Input] = {
+    plan.collect {
+      case r: HiveTableRelation if r.outputSet.contains(attr) => Seq(HiveInput(r.tableMeta.qualifiedName, Set(Field(attr.name, how))))
+      case r: LogicalRelation if r.outputSet.contains(attr) => Seq(HiveInput(r.catalogTable.get.qualifiedName, Set(Field(attr.name, how))))
+      case j: Join => {
+        val conds = j.condition.map { cond =>
+          cond.collect {
+            case ar: AttributeReference => j.children.flatMap({ child => findSource(ar, child, How.JOIN) })
+            case al: Alias => al.collect {
+              case ar: AttributeReference => j.children.flatMap({ child => findSource(ar, child, How.JOIN) })
+            }.flatten
+          }.flatten
+        }
+          .getOrElse(Seq())
+          .filter({ input => input.fields.map({ f => f.how == How.JOIN }).reduce((a, b) => a || b) })
+
+        j.children.flatMap({ child => findSource(attr, child, null) ++ conds })
+      }
+      case p: Project => {
+        p.projectList.flatMap { proj =>
+          proj.collect {
+            case ar: AttributeReference if ar.toAttribute == attr => findSource(ar, p.child, How.PROJECTION)
+            case al: Alias if al.name == attr.name => al.collect {
+              case ar: AttributeReference => findSource(ar, p.child, How.PROJECTION)
+            }.flatten
+          }.flatten
+        }
+      }
+      case f: Filter => f.condition.collect {
+        case ar: AttributeReference => findSource(ar, f.child, How.FILTER)
+        case _ => Seq()
+      }.flatten
+      case a: Aggregate => {
+        val groupings = a.groupingExpressions.collect {
+          case ar: AttributeReference => findSource(ar, a.child, How.GROUPBY)
+          case al: Alias => al.collect {
+            case ar: AttributeReference => findSource(ar, a.child, How.GROUPBY)
+          }.flatten
+        }.flatten
+
+        val aggregates = a.aggregateExpressions.diff(a.groupingExpressions).collect {
+          case ar: AttributeReference if ar.toAttribute == attr => findSource(ar, a.child, How.AGGREGATE) ++ groupings
+          case al: Alias if al.name == attr.name => al.collect {
+            case ar: AttributeReference => findSource(ar, a.child, How.AGGREGATE) ++ groupings
+          }.flatten
+        }.flatten
+
+        aggregates ++ groupings
+      }
+    }.flatten
+  }
+
+  def parseQuery(qe: QueryExecution): Option[Report] = {
+    qe.logical.collectFirst {
+      case c: InsertIntoHadoopFsRelationCommand => {
+        val output = FsOutput(c.outputPath.toString, c.fileFormat.toString)
+        val inputs = c.query.output.map {
+          attr => attr -> findSource(attr, c.query, null)
+        }.map({ case (field, rawInputs) => {
+          val inputs = rawInputs.groupBy {
+            case HiveInput(name, _, _) => (HiveInput, name)
+          }.map { case (key, value) => {
+            key match {
+              case (HiveInput, table) =>
+                HiveInput(
+                  table,
+                  value
+                    .asInstanceOf[Seq[HiveInput]]
+                    .map(_.fields.filter({ col => col.how != null }))
+                    .reduce((a, b) => a ++ b))
+            }
+          }
+          }
+
+          field.name -> inputs.toList
+        }
+        })
+
+        LOGGER.info(s"Inputs: $inputs")
+
+        val metadata = Metadata(qe.sparkSession.sparkContext.appName)
+
+        Report(metadata, output, Map(inputs: _*))
+      }
+    }
+  }
+}
--- src/main/scala/org/chojin/spark/lineage/SparkSqlLineageListener.scala
+++ src/main/scala/org/chojin/spark/lineage/SparkSqlLineageListener.scala
@@ -1,16 +1,8 @@
 package org.chojin.spark.lineage
 
 import grizzled.slf4j.Logger
-import org.apache.spark.sql.catalyst.catalog.HiveTableRelation
-import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeReference}
-import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.execution.QueryExecution
-import org.apache.spark.sql.execution.datasources.{InsertIntoHadoopFsRelationCommand, LogicalRelation}
 import org.apache.spark.sql.util.QueryExecutionListener
-import org.chojin.spark.lineage.inputs.How.How
-import org.chojin.spark.lineage.inputs.{Field, HiveInput, How, Input}
-import org.chojin.spark.lineage.outputs.FsOutput
-import org.chojin.spark.lineage.report.{Metadata, Report}
 import org.chojin.spark.lineage.reporter.Reporter
 
 class SparkSqlLineageListener(reporter: Reporter) extends QueryExecutionListener {
@@ -18,99 +10,10 @@ class SparkSqlLineageListener(reporter: Reporter) extends QueryExecutionListener
 
   def this() = this(Config.createInstanceOf("reporter"))
 
-  def findSource(attr: AttributeReference, plan: LogicalPlan, how: How): Seq[Input] = findSource(attr.toAttribute, plan, how)
-
-  def findSource(attr: Attribute, plan: LogicalPlan, how: How): Seq[Input] = {
-    plan.collect {
-      case r: HiveTableRelation if r.outputSet.contains(attr) => Seq(HiveInput(r.tableMeta.qualifiedName, Set(Field(attr.name, how))))
-      case r: LogicalRelation if r.outputSet.contains(attr) => Seq(HiveInput(r.catalogTable.get.qualifiedName, Set(Field(attr.name, how))))
-      case j: Join => {
-        val conds = j.condition.map { cond =>
-          cond.collect {
-            case ar: AttributeReference => j.children.flatMap({ child => findSource(ar, child, How.JOIN) })
-            case al: Alias => al.collect {
-              case ar: AttributeReference => j.children.flatMap({ child => findSource(ar, child, How.JOIN) })
-            }.flatten
-          }.flatten
-        }
-          .getOrElse(Seq())
-          .filter({ input => input.fields.map({ f => f.how == How.JOIN }).reduce((a, b) => a || b) })
-
-        j.children.flatMap({ child => findSource(attr, child, null) ++ conds })
-      }
-      case p: Project => {
-        p.projectList.flatMap { proj =>
-          proj.collect {
-            case ar: AttributeReference if ar.toAttribute == attr => findSource(ar, p.child, How.PROJECTION)
-            case al: Alias if al.name == attr.name => al.collect {
-              case ar: AttributeReference => findSource(ar, p.child, How.PROJECTION)
-            }.flatten
-          }.flatten
-        }
-      }
-      case f: Filter => f.condition.collect {
-        case ar: AttributeReference => findSource(ar, f.child, How.FILTER)
-        case _ => Seq()
-      }.flatten
-      case a: Aggregate => {
-        val groupings = a.groupingExpressions.collect {
-          case ar: AttributeReference => findSource(ar, a.child, How.GROUPBY)
-          case al: Alias => al.collect {
-            case ar: AttributeReference => findSource(ar, a.child, How.GROUPBY)
-          }.flatten
-        }.flatten
-
-        val aggregates = a.aggregateExpressions.diff(a.groupingExpressions).collect {
-          case ar: AttributeReference if ar.toAttribute == attr => findSource(ar, a.child, How.AGGREGATE) ++ groupings
-          case al: Alias if al.name == attr.name => al.collect {
-            case ar: AttributeReference => findSource(ar, a.child, How.AGGREGATE) ++ groupings
-          }.flatten
-        }.flatten
-
-        aggregates ++ groupings
-      }
-    }.flatten
-  }
-
-  def run(qe: QueryExecution): Option[Report] = {
-    qe.logical.collectFirst {
-      case c: InsertIntoHadoopFsRelationCommand => {
-        val output = FsOutput(c.outputPath.toString, c.fileFormat.toString)
-        val inputs = c.query.output.map {
-          attr => attr -> findSource(attr, c.query, null)
-        }.map({ case (field, rawInputs) => {
-          val inputs = rawInputs.groupBy {
-            case HiveInput(name, _, _) => (HiveInput, name)
-          }.map { case (key, value) => {
-            key match {
-              case (HiveInput, table) =>
-                HiveInput(
-                  table,
-                  value
-                    .asInstanceOf[Seq[HiveInput]]
-                    .map(_.fields.filter({ col => col.how != null }))
-                    .reduce((a, b) => a ++ b))
-            }
-          }
-          }
-
-          field.name -> inputs.toList
-        }
-        })
-
-        LOGGER.info(s"Inputs: $inputs")
-
-        val metadata = Metadata(qe.sparkSession.sparkContext.appName)
-
-        Report(metadata, output, Map(inputs: _*))
-      }
-    }
-  }
-
   override def onSuccess(funcName: String, qe: QueryExecution, durationNs: Long): Unit = {
     qe.sparkSession.sparkContext.appName
     LOGGER.debug(s"Logical plan: ${qe.logical}")
-    run(qe).foreach(report => {
+    QueryParser.parseQuery(qe).foreach(report => {
       LOGGER.debug(s"Produced report: ${report.prettyPrint}")
       reporter.report(report)
     })
